ee1dc9b82cb3182970d3a5a48c803a26b7d39004
elfer07 <morenofernando@gmail.com>
2025-04-24 16:04:39 -0300

    Initial Commit

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Fernando Moreno on 24/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
